d8f0df4ce5a66b5fda25552deaa58f08b3e64447
Max <89795178+maxmandia@users.noreply.github.com>
2022-03-08 11:59:39 -0500

    Completed js exercise 03 (reversed string)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(stringy) {
+    let split = stringy.split("");
+    let reverse = split.reverse(); 
+    let final = reverse.join("");
+    return final;
 };
 
 // Do not edit below this line
